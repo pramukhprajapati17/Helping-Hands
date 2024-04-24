64a07d0a05f038db59f834b312f22e37818d2d43
pramukhprajapati17 <pramukhprajapati17@gmail.com>
2024-04-25 00:39:10 +0530

    Responsive + nav bar anchore tag src updated

--- Js/index.js
+++ Js/index.js
@@ -1,1 +1,13 @@
-alert("Hii How are you?");
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navbar');
+leftnav = document.querySelector('.leftnav');
+rightnav = document.querySelector('.rightnav');
+
+
+burger.addEventListener('click', () =>{
+    rightnav.classList.toggle('v-class');
+    
+    leftnav.classList.toggle('v-class');
+    
+    navbar.classList.toggle('h-nav');
+})
